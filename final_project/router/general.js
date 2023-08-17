9490253eef77d91a63cdc24b2abfa67f513b4d62
Delta-Hat <sdhmob@gmail.com>
2023-08-17 01:54:57 -0400

    added some get method stuff

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,14 +19,29 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    let book = books[isbn];
+    if(book){
+        return res.status(200).send(JSON.stringify(book));
+    }
+    return res.status(404).send("Book not found.");
+    //return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let author = req.params.author
+    let keys = Object.keys(books);
+    let results = [];
+    for(let i = 0; i < keys.length; i++){
+        let key = keys[i];
+        let book = books[key];
+        if(book.author === author){
+            results.push(book);
+        }
+    }
+    //let results = books.filter((book) => book.author == author);
+    return res.status(200).send(results);
 });
 
 // Get all books based on title
